c9bc8c187c72dc0252319299c281a2f9a1f236b1
Maja Repinc <maja@Majas-MacBook.local>
2019-02-25 15:58:37 +0000

    added window module drag and drop

--- js/script.js
+++ js/script.js
@@ -2,17 +2,92 @@
 document.addEventListener("DOMContentLoaded", function(event) {
   funStartTime();
   document.getElementById("icon-expand").onclick = function() {
-      var isInFullScreen = (document.fullscreenElement && document.fullscreenElement !== null) ||
-        (document.webkitFullscreenElement && document.webkitFullscreenElement !== null) ||
-        (document.mozFullScreenElement && document.mozFullScreenElement !== null) ||
-        (document.msFullscreenElement && document.msFullscreenElement !== null);
-       if (!isInFullScreen) {
-           funOpenFullscreen();
-       } else {
-           funCloseFullscreen();
-       }
+      funFullscreen();
   };
 });
+
+function funFullscreen() {
+    var isInFullScreen = (document.fullscreenElement && document.fullscreenElement !== null) ||
+      (document.webkitFullscreenElement && document.webkitFullscreenElement !== null) ||
+      (document.mozFullScreenElement && document.mozFullScreenElement !== null) ||
+      (document.msFullscreenElement && document.msFullscreenElement !== null);
+     if (!isInFullScreen) {
+         funOpenFullscreen();
+     } else {
+         funCloseFullscreen();
+     }
+}
+
+var Draggable = function (id) {
+    var el = document.querySelector(id),
+        isDragReady = false,
+        dragoffset = {
+            x: 0,
+            y: 0
+        };
+    this.init = function () { this.events(); };
+
+    //events for the element
+    this.events = function () {
+        var self = this;
+        _on(document.getElementsByClassName('modal-header')[0], 'mousedown', function (e) {
+            isDragReady = true;
+            e.preventDefault();
+            var modalMousedown = document.querySelectorAll(".modal-wrapper")
+            for (var count = 0; count < modalMousedown.length; count++) {
+              modalMousedown[count].style.zIndex = 1;
+            }
+            e.target.closest(".modal-wrapper").style.zIndex = 100;
+            //corssbrowser mouse pointer values
+            e.pageX = e.pageX || e.clientX + (document.documentElement.scrollLeft ? document.documentElement.scrollLeft : document.body.scrollLeft);
+            e.pageY = e.pageY || e.clientY + (document.documentElement.scrollTop ? document.documentElement.scrollTop : document.body.scrollTop);
+            dragoffset.x = e.pageX - el.offsetLeft;
+            dragoffset.y = e.pageY - el.offsetTop;
+        });
+        _on(document.getElementsByClassName('modal-header')[0], 'mouseup', function () {
+            isDragReady = false;
+            var modalMouseup = document.querySelectorAll(".modal-wrapper")
+            if(modalMouseup.length <= 1) {
+                for (var count = 0; count < modalMouseup.length; count++) {
+                  modalMouseup[count].style.zIndex = 1;
+                }
+            }
+        });
+        _on(document, 'mousemove', function (e) {
+            if (isDragReady) {
+                e.pageX = e.pageX || e.clientX + (document.documentElement.scrollLeft ? document.documentElement.scrollLeft : document.body.scrollLeft);
+                e.pageY = e.pageY || e.clientY + (document.documentElement.scrollTop ? document.documentElement.scrollTop : document.body.scrollTop);
+                // left/right constraint
+                if (e.pageX - dragoffset.x < 0) {
+                    offsetX = 0;
+                } else if (e.pageX - dragoffset.x + el.offsetWidth > document.body.clientWidth) {
+                    offsetX = document.body.clientWidth - el.offsetWidth;
+                } else {
+                    offsetX = e.pageX - dragoffset.x;
+                }
+
+                // top/bottom constraint
+                if (e.pageY - dragoffset.y < 40) {
+                    offsetY = 40;
+                } else if (e.pageY - dragoffset.y + el.offsetHeight > document.body.clientHeight) {
+                    offsetY = document.body.clientHeight - el.offsetHeight;
+                } else {
+                    offsetY = e.pageY - dragoffset.y;
+                }
+
+                el.style.top = offsetY + "px";
+                el.style.left = offsetX + "px";
+            }
+        });
+    };
+    //cross browser event Helper function
+    var _on = function (el, event, fn) {
+        document.attachEvent ? el.attachEvent('on' + event, fn) : el.addEventListener(event, fn, !0);
+    };
+    this.init();
+}
+
+//new Draggable('#icon-about .modal-wrapper');
 
 //display date and time
 function funStartTime() {
